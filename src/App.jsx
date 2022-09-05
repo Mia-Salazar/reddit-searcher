72b581e2bae6b772f0f91d9ad2f91ab03ebbe453
Mia-Salazar <maria.sgr@gmail.com>
2022-09-05 18:33:32 +0200

    Add map page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Header } from "./components/Header/Header";
 import Home from "./page/Home/Home";
 import ByDay from "./page/ByDay/ByDay";
+import Map from "./page/Map/Map";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}  />
           <Route path="/day" element={<ByDay />}  />
+          <Route path="/map" element={<Map />}  />
         </Routes>
       </main>
     </BrowserRouter>
