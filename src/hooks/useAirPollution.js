35baa7c281c8fc0d0990e008055efa423a925308
Mia-Salazar <maria.sgr@gmail.com>
2022-09-10 15:00:30 +0200

    Add API to get city name

--- src/hooks/useAirPollution.js
+++ src/hooks/useAirPollution.js
@@ -5,10 +5,11 @@ const useAirPollution = (data) => {
     const [loading, setLoading] = useState(true);
     const apiKey = "33a0a36aa4fc08982e82b8e705e2e822";
     useEffect(() => {
+        //http://api.openweathermap.org/data/2.5/air_pollution/history?lat=${data.lat}&lon=${data.lon}&start=${data.start}&end=${data.end}&appid=${apiKey}
         //http://api.openweathermap.org/data/2.5/air_pollution/history?lat=50&lon=50&start=1606223802&end=1606482999&appid=${apiKey}
         if (data) {
             setLoading(true)
-            fetch(`http://api.openweathermap.org/data/2.5/air_pollution/history?lat=${data.lat}&lon=${data.lon}&start=${data.start}&end=${data.end}&appid=${apiKey}`)
+            fetch(`http://api.openweathermap.org/data/2.5/air_pollution/history?lat=50&lon=50&start=1606223802&end=1606482999&appid=${apiKey}}`)
             .then(response => response.json())
             .then(response => {
                 setPollution(response);
