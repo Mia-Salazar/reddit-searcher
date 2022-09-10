90138cf21403b80d3c394fc64735448caa0bed56
Mia-Salazar <maria.sgr@gmail.com>
2022-09-10 13:32:38 +0200

    Create pollution form

--- src/components/SearchMap/SearchMap.jsx
+++ src/components/SearchMap/SearchMap.jsx
@@ -14,6 +14,7 @@ export const SearchMap = ({setData}) => {
             ...dataInput,
             [event.target.name] : event.target.value
         })
+        console.log(dataInput)
     }
     const selectOptions = [
         {value: "", text: "Select..."},
--- src/page/Pollution/Pollution.jsx
+++ src/page/Pollution/Pollution.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 
 import SearchPollution from "../../components/SearchPollution/SearchPollution";
+import useAirPollution from "../../hooks/useAirPollution";
 
 export const Pollution = () => {
 	const [data, setData] = useState();
-    //const {weather, loading} = useMapWeather(data);
+    const {pollution, loading} = useAirPollution(data);
 	return (
 		<section >
 			<article className="nes-container with-title">
 				<h2 className="title">Air Pollution</h2>
-                <SearchPollution />
+                <SearchPollution setData={setData} />
+				{loading && <h2>Cargando...</h2>}
 			</article>
 		</section>
 	);
